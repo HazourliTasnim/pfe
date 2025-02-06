268bd8986ebb093c48b46dc9c5bd735bcd82465e
HazourliTasnim <tasnim.hazourli@gmail.com>
2025-02-06 15:39:59 +0100

    init

---
+++ frontend/src/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const users = ref([]);
+
+onMounted(async () => {
+    const response = await axios.get('/users'); // Récupère les utilisateurs depuis Laravel
+    users.value = response.data;
+});
+</script>
+
+<template>
+  <div>
+    <h1>Liste des utilisateurs</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
